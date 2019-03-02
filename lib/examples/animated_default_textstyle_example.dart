545b79cb2ff14821337d39288476841c78a78895
Stefan Mitev <mr.mitew@gmail.com>
2019-03-02 11:00:45 +0100

    [add] Animating changes in Text's TextStyle using AnimatedDefaultTextStyle

---
+++ lib/examples/animated_default_textstyle_example.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class AnimatedDefaultTextStyleExample extends StatefulWidget {
+  @override
+  _AnimatedDefaultTextStyleExampleState createState() =>
+      _AnimatedDefaultTextStyleExampleState();
+}
+
+class _AnimatedDefaultTextStyleExampleState
+    extends State<AnimatedDefaultTextStyleExample> {
+  bool _flag = false;
+  TextStyle _textStyle;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _textStyle = Theme.of(context).textTheme.display2.apply(color: Colors.blue);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => setState(() {
+            _flag = !_flag;
+            _textStyle = (_flag)
+                ? Theme.of(context)
+                    .textTheme
+                    .display4
+                    .apply(color: Colors.green)
+                    .copyWith(fontWeight: FontWeight.bold)
+                : Theme.of(context)
+                    .textTheme
+                    .display2
+                    .apply(color: Colors.blue);
+          }),
+      child: Center(
+        child: AnimatedDefaultTextStyle(
+          curve: Curves.elasticOut,
+          style: _textStyle,
+          duration: Duration(milliseconds: 500),
+          child: Text("Hello"),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animations/examples/animated_container_example.dart';
+import 'package:flutter_animations/examples/animated_default_textstyle_example.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AnimatedContainerExample(),
+      home: AnimatedDefaultTextStyleExample(),
     );
   }
 }
