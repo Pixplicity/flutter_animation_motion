af6a9830bcd342e064d1396e09f72c582551d61f
Stefan Mitev <mr.mitew@gmail.com>
2019-03-02 10:57:07 +0100

    [add] Animating changes in container's properties using AnimatedContainer

---
+++ lib/examples/animated_container_example.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class AnimatedContainerExample extends StatefulWidget {
+  @override
+  _AnimatedContainerExampleState createState() =>
+      _AnimatedContainerExampleState();
+}
+
+class _AnimatedContainerExampleState extends State<AnimatedContainerExample> {
+  var _width = 50.0;
+  var _height = 50.0;
+  var _borderRadius = 0.0;
+  var _color = Colors.blue[400];
+  var _tapCount = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        onTap: _changeStateOnTap,
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 1500),
+          curve: Curves.elasticOut,
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            color: _color,
+            borderRadius: BorderRadius.circular(_borderRadius),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _changeStateOnTap() {
+    setState(() {
+      _tapCount++;
+      if (_tapCount == 1) {
+        _setState1();
+      } else if (_tapCount == 2) {
+        _setState2();
+      } else if (_tapCount == 3) {
+        _setState3();
+        _reset();
+      }
+    });
+  }
+
+  void _reset() {
+    _tapCount = 0;
+  }
+
+  void _setState3() {
+    _width = 80.0;
+    _height = 80.0;
+    _color = Colors.green[400];
+    _borderRadius = 50.0;
+  }
+
+  void _setState2() {
+    _width = 50.0;
+    _height = 50.0;
+    _borderRadius = 0.0;
+    _color = Colors.blue[400];
+  }
+
+  void _setState1() {
+    _width = 150.0;
+    _height = 150.0;
+    _borderRadius = 20.0;
+    _color = Colors.pink[400];
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animations/examples/animated_container_example.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: AnimatedContainerExample(),
     );
   }
 }
