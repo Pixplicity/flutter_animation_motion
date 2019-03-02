7207b95631a8d89172f159372651312882ad20fe
Stefan Mitev <mr.mitew@gmail.com>
2019-03-02 11:23:16 +0100

    [add] Change perspective of a Widget using Transform widget

---
+++ lib/examples/static_transform_perspective_example.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class StaticTransformPerspectiveExample extends StatefulWidget {
+  @override
+  _StaticTransformPerspectiveExampleState createState() =>
+      new _StaticTransformPerspectiveExampleState();
+}
+
+class _StaticTransformPerspectiveExampleState
+    extends State<StaticTransformPerspectiveExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Transform(
+      transform: Matrix4.identity()
+        ..setEntry(3, 2, 0.01)
+        ..rotateX(-0.2),
+      alignment: Alignment.center,
+      child: Center(
+        child: Icon(
+          Icons.adb,
+          color: Colors.green,
+          size: 300,
+        ),
+      ),
+    );
+  }
+}
