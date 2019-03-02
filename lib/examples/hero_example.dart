e74a894d126ccdfd2857b60a330fa5664f22ee24
Stefan Mitev <mr.mitew@gmail.com>
2019-03-02 11:11:43 +0100

    [add] Hero transition example using a Hero widget

---
+++ lib/examples/hero_example.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class HeroExampleMain extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white30,
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        alignment: Alignment.bottomRight,
+        child: Hero(
+          tag: "demo",
+          child: GestureDetector(
+            onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext ctx) => HeroExampleDetails())),
+            child: Icon(
+              Icons.favorite,
+              size: 40,
+              color: Colors.pink[400],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HeroExampleDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.topCenter,
+        child: Hero(
+          tag: "demo",
+          child: Icon(
+            Icons.favorite,
+            size: 300,
+            color: Colors.pink[400],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animations/examples/animatedpositioned_example.dart';
 import 'package:flutter_animations/examples/animatedwidget_opacity_example.dart';
 import 'package:flutter_animations/examples/decoratedbox_transition_example.dart';
+import 'package:flutter_animations/examples/hero_example.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DecoratedBoxTransitionExample(),
+      home: HeroExampleMain(),
     );
   }
 }
